819475fa233bbcc2a0ab8f4e465b58be4bbefa12
Robert Hammock <rhamm1027@gmail.com>
2021-02-04 15:38:04 -0500

    insert new job service

--- src/jobs/jobs-service.js
+++ src/jobs/jobs-service.js
@@ -1,6 +1,15 @@
 const jobsSerivice = {
+
     getAllJobs(knex) {
         return knex.select('*').from('jobs');
+    },
+
+    insertNewJob(knex, newJob) {
+        return knex
+            .insert(newJob)
+            .into('jobs')
+            .returning('*')
+            .then((rows) => rows[0]);
     }
 }
 
