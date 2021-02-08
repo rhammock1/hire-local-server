b87bd8ff40dc53495f3536aae751eb5a41e43873
Robert Hammock <rhamm1027@gmail.com>
2021-02-08 18:22:04 -0500

    getSave and delete save service

--- src/saves/saves-service.js
+++ src/saves/saves-service.js
@@ -12,6 +12,20 @@ const savesService = {
             .returning('*')
             .then((rows) => rows[0]);
     },
+    getById(knex, saveId) {
+        return knex
+            .from('user_saves')
+            .select('*')
+            .where('id', saveId)
+            .first();
+    },
+    delete(knex, saveId) {
+        return knex 
+            .from('user_saves')
+            .select('*')
+            .where('id', saveId)
+            .delete();
+    }
 }
 
 module.exports = savesService;
