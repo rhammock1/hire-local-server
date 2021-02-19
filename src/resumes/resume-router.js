dda74d95d82a22c38cfe31468878b4c04f50bda0
Robert Hammock <rhamm1027@gmail.com>
2021-02-19 14:37:24 -0500

    replaced depreciated Buffer

--- src/resumes/resume-router.js
+++ src/resumes/resume-router.js
@@ -21,8 +21,8 @@ resumeRouter
                 if (!resume) {
                     return res.status(404).json({ error: 'No resume found for the selected user'});
                 }
-                const fileData = new Buffer(resume.resume); 
-                // fs.writeFile(resume.original_name, fileData, (err) => (err) ? console.error(err) : null);
+                const fileData = Buffer.from(resume.resume); 
+                
                 res.writeHead(200, { 'Content-Type': 'application/pdf', 'Content-Disposition': `attachment; filename=${resume.original_name}`, 'Content-Length': fileData.length }); 
                 res.write(fileData);
                 
